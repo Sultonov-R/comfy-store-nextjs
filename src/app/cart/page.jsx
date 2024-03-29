33d789a2456a211b66b22f42ef035119c73d9862
Sultonov-R <sultonovrozali@gmail.com>
2024-03-29 06:28:17 +0500

    error page and cart page are added

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,12 @@
+ import styles from '../page.module.css';
+
+function Cart(){
+    return(
+        <>
+        <div className={styles.cart_page}>
+            <h1>Cart</h1>
+        </div>
+        </>
+    )
+}
+export default Cart
---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+function NotFound() {
+  return (
+    <div className={styles.error_page}><h1>Error Page</h1></div>
+  )
+}
+
+export default NotFound
